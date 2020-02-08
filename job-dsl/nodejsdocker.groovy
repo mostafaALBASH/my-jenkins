dc6df46bbcfcdfa72f5b05ee3c27548f113f1bfa
mostafaALBASH <mostafa_elbesh@outlook.com>
2020-02-08 16:58:38 +0400

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/mostafaALBASH/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            node / gitConfigEmail('mostafa_elbesh@outlook.com')
         }
     }
     triggers {
